18a7f80ddd7a0aff671267aafad62154f91dfdee
Henadzi Arashkevich <oreshkevich85@mail.ru>
2021-11-04 17:41:39 +0300

    feat: add task-03

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+const answer = path.join(__dirname, "./secret-folder/");
+fs.readdir(
+  answer,
+  {
+    withFileTypes: true,
+  },
+  (err, files) => {
+    for (var i = 0; i < files.length; i++) {
+      if (files[i].isFile()) {
+        for (let key in files[i]) {
+          // console.log(files[i][key]);
+          var file = answer + "/" + files[i][key];
+          fs.stat(file, callback(file));
+        }
+      }
+    }
+  }
+);
+function callback(file) {
+  return function (err, stats) {
+    console.log(
+      `${file
+        .replace(/^.*[\\\/]/, "")
+        .split(".")
+        .slice(0, 1)} - ${file.split(".").pop()} - ${stats["size"] / 1000}kb`
+    );
+  };
+}
