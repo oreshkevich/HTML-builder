199b67fbdeaf054a90b31306d590a300b0a0054d
Henadzi Arashkevich <oreshkevich85@mail.ru>
2021-11-05 15:27:58 +0300

    feat: add answer task 04-copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+const fCopy = path.resolve("04-copy-directory", "files-copy");
+const file = path.resolve("04-copy-directory", "files");
 
+fs.access(fCopy, (err) => {
+  if (err) {
+    copy(file, fCopy);
+  } else {
+    fs.promises.rm(fCopy, { recursive: true });
+    copy(file, fCopy);
+  }
+});
+
+let child = require("child_process");
+function copy(was, become) {
+  was = was.replace(/\//gim, "\\");
+  become = become.replace(/\//gim, "\\");
+  child.exec('xcopy /y /q "' + was + '\\*" "' + become + '\\"');
+}
